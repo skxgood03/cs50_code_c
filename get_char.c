b1c7cbe149e4e20a88d767bef370dff705b37634
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-13 08:42:55 +0000

    Wed, Nov 13, 2024, 8:42 AM +00:00

--- get_char.c
+++ get_char.c
@@ -4,9 +4,9 @@
 int main()
 {
 	char x = get_char("y or n: ");
-    if(x =="Y" || x=="y"){
+    if(x =='Y' || x=='y'){
         printf("yes\n");
-    }else if(x =="N" || x=="n"){
+    }else if(x =='N' || x=='n'){
        printf("no\n");
     }else{
         printf("都不是\n");
