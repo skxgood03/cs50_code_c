a6eeb859969f45a5648a61be54bc0f7c0fdaf253
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-13 07:14:30 +0000

    Wed, Nov 13, 2024, 7:14 AM +00:00

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main()
+{
+	string name = get_string("What is your name?\n");
+
+	printf("Hello, %s!\n", name);
+	printf("Hello, World!\n");
+	return 0;
+}
