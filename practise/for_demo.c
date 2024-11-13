e28df40bbb3fa9889f16761218b87bdfbd0998cb
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-13 09:17:11 +0000

    Wed, Nov 13, 2024, 9:17 AM +00:00

--- practise/for_demo.c
+++ practise/for_demo.c
@@ -1,6 +1,9 @@
 #include<cs50.h>
 #include<stdio.h>
 
+//必须把调用的函数写在调用者上面 ，或者将函数第一行写在上面如下
+void one_fun();
+
 int main()
 {
     for (int i = 0; i<3; i++){
@@ -8,4 +11,6 @@ int main()
     }
 }
 
-
+void one_fun(){
+    printf("hello，this is one\n");
+}
