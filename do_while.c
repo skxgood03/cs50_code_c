32c56aeb5f9fac97f853d7e3224c14332b1cc353
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-13 10:32:29 +0000

    Wed, Nov 13, 2024, 6:32 PM +08:00

--- do_while.c
+++ do_while.c
@@ -1,19 +1,21 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
 int do_while_fun(string prompt);
 
 int main()
 {
-   int i =  do_while_fun("请输入一个数字: ");
-   printf("%i\n",i);
+    int i = do_while_fun("请输入一个数字: ");
+    printf("%i\n", i);
 }
 
-int ddo_while_fun(string prompt){
+int do_while_fun(string prompt)
+{
     int n;
-    do{
+    do
+    {
         n = get_int(prompt);
     }
-    while(n<1);
+    while (n < 1);
     return n;
 }
