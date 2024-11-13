8e35ca61ae651c622679e67580c635ef68778463
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-13 08:10:29 +0000

    Wed, Nov 13, 2024, 8:10 AM +00:00

--- practise/calculate_floats.c
+++ practise/calculate_floats.c
@@ -5,9 +5,9 @@ int main(){
     float x =  get_float("x:");
     float y =  get_float("y:");
 
-    printf("x+y=%i\n",x+y);
-    printf("x-y=%i\n",x-y);
-    printf("x*y=%i\n",x*y);
-    printf("x/y=%i\n",x/y);
+    printf("x+y=%f\n",x+y);
+    printf("x-y=%f\n",x-y);
+    printf("x*y=%f\n",x*y);
+    printf("x/y=%f\n",x/y);
 
 }
