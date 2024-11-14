49d672b1caf4640523b80f93886ceed2a06537c5
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-14 13:50:21 +0000

    Thu, Nov 14, 2024, 9:50 PM +08:00

--- practise/first_lesson_assignment.c
+++ practise/first_lesson_assignment.c
@@ -6,6 +6,10 @@ int main(){
     float tip = get_float("Bill before tax and tip: ");
     float percent = get_float("Sale Tax Percent: ");
     float tip_percent = get_float("Tip percent: ");
-    
+    float tax_amount = tip*percent+tip;
+    float tipping = tax_amount*tip_percent;
+    float bill_per_person = (tax_amount+tipping)/2;
+    printf("You will owe $%.2f each!",bill_per_person);
+    return 0;
 
 }
