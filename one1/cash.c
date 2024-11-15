5793ab15db1bf66ea186f375e898ef7a9195a4a7
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 09:27:15 +0000

    Fri, Nov 15, 2024, 5:27 PM +08:00

--- one1/cash.c
+++ one1/cash.c
@@ -1,27 +1,31 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int calculate_quarters(int n);
+int calculate_quarters(int n,int i);
 int main(void)
 {
     int n;
     int frequency;
+    int num, many;
     do
     {
         n = get_int("请输入需要找零的金额：");
     }
     while (n < 0);
-    printf("%i\n", calculate_quarters(n));
-    if(n>=25){
-       frequency +=  calculate_quarters(n,25);
-    }else if(n>=10){
-        frequency +=  calculate_quarters(n,10);
-    }else if(n>=5){
-        frequency +=  calculate_quarters(n,5);
+    if (n >= 25)
+    {
+        num, many = calculate_quarters(n, 25);
     }
-    else{
-        frequency +=  calculate_quarters(n,1);
-    }
+    printf("%i还有%i\n，num,many");
+
+    // }else if(n>=10<25){
+    //     frequency +=  calculate_quarters(n,10);
+    // }else if(n>=5<10){
+    //     frequency +=  calculate_quarters(n,5);
+    // }
+    // else{
+    //     frequency +=  calculate_quarters(n,1);
+    // }
 }
 
 int calculate_quarters(int n,int i)
@@ -33,5 +37,5 @@ int calculate_quarters(int n,int i)
         many = many - i;
         num++;
     }
-    return num;
+    return num, many;
 }
