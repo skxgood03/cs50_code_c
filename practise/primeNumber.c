05b4e9181a247d457538472e3ab201ef212f59be
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 03:00:11 +0000

    Fri, Nov 15, 2024, 11:00 AM +08:00

--- practise/primeNumber.c
+++ practise/primeNumber.c
@@ -6,13 +6,17 @@ int main(){
     int max  = get_int("请输入最大数：");
     for(int i = min; i<=max;i++ ){
         printf("%i",i);
+       bool over=get_bool prime(i)
     }
 
 }
 
 prime(int i){
     for(int u=0; u<i; u++){
-        
+        if (n % i == 0){
+             return False
+        }
+
     }
 }
 
