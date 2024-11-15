fe9de120a42b2441a511108d3397ee0f6786c9e4
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 09:20:55 +0000

    Fri, Nov 15, 2024, 5:20 PM +08:00

--- one1/cash.c
+++ one1/cash.c
@@ -5,21 +5,32 @@ int calculate_quarters(int n);
 int main(void)
 {
     int n;
+    int frequency;
     do
     {
         n = get_int("请输入需要找零的金额：");
     }
     while (n < 0);
     printf("%i\n", calculate_quarters(n));
+    if(n>=25){
+       frequency +=  calculate_quarters(n,25);
+    }else if(n>=10){
+        frequency +=  calculate_quarters(n,10);
+    }else if(n>=5){
+        frequency +=  calculate_quarters(n,5);
+    }
+    else{
+        frequency +=  calculate_quarters(n,1);
+    }
 }
 
-int calculate_quarters(int n)
+int calculate_quarters(int n,int i)
 {
     int num = 0;
     int many = n;
-    while (many >= 25)
+    while (many >= i)
     {
-        many = many - 25;
+        many = many - i;
         num++;
     }
     return num;
