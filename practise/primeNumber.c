7af5cda8db0db6a5d6bc657768c16c3d5554d407
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 06:12:12 +0000

    Fri, Nov 15, 2024, 2:12 PM +08:00

--- practise/primeNumber.c
+++ practise/primeNumber.c
@@ -1,36 +1,42 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
 bool prime(int i);
-int main(){
-    int min  = get_int("请输入最小数：");
-    int max  = get_int("请输入最大数：");
-    for(int i = min; i<=max;i++ ){
-        if(prime(i)){
-            printf("%i\n",i);
+int main()
+{
+    int min = get_int("请输入最小数：");
+    int max = get_int("请输入最大数：");
+    for (int i = min; i <= max; i++)
+    {
+        if (prime(i))
+        {
+            printf("%i\n", i);
         }
     }
-
 }
 
-bool prime(int i){
-    if(i<=1){
+bool prime(int i)
+{
+    if (i <= 1)
+    {
         return false;
     }
-    if(i==2){
+    if (i == 2)
+    {
         return true;
     }
     // 如果数值是偶数且大于 2，则不是质数
-    if (i % 2 == 0) {
+    if (i % 2 == 0)
+    {
         return false;
     }
     float y = 0.0;
-    for(int u=3; u<i; u++){
-         if (i % u == 0) {
+    for (int u = 3; u < i; u++)
+    {
+        if (i % u == 0)
+        {
             return false;
         }
     }
-     return true;
-
+    return true;
 }
-
