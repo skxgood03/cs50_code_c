905f45faf87fd8260a220c9f5950d79b92ceebd3
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 02:47:21 +0000

    Fri, Nov 15, 2024, 10:47 AM +08:00

--- practise/primeNumber.c
+++ practise/primeNumber.c
@@ -4,7 +4,13 @@
 int main(){
     int min  = get_int("请输入最小数：");
     int max  = get_int("请输入最大数：");
-    for(int i = min; i<=max; ){
+    for(int i = min; i<=max;i++ ){
         printf("%i",i);
     }
 }
+
+prime(int i){
+
+}
+5
+1
