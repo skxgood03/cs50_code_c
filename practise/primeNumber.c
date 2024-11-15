1d873d648b91b7eb1789b26fe48fb8ca9dea64b9
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 03:03:08 +0000

    Fri, Nov 15, 2024, 11:03 AM +08:00

--- practise/primeNumber.c
+++ practise/primeNumber.c
@@ -1,20 +1,27 @@
 #include<cs50.h>
 #include<stdio.h>
 
+bool prime(int i);
 int main(){
     int min  = get_int("请输入最小数：");
     int max  = get_int("请输入最大数：");
     for(int i = min; i<=max;i++ ){
-        printf("%i",i);
-       bool over=get_bool prime(i)
+
+        bool over= prime(i);
+        if(over){
+            printf("%i",i);
+        }
     }
 
 }
 
-prime(int i){
+bool prime(int i){
+    if(i==1){
+        return False;
+    }
     for(int u=0; u<i; u++){
         if (n % i == 0){
-             return False
+             return False;
         }
 
     }
