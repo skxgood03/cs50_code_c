1e07967ea21606096a119bb8c2df5c965f1d88dd
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-15 06:04:43 +0000

    Fri, Nov 15, 2024, 2:04 PM +08:00

--- practise/primeNumber.c
+++ practise/primeNumber.c
@@ -7,31 +7,30 @@ int main(){
     int max  = get_int("请输入最大数：");
     for(int i = min; i<=max;i++ ){
         if(prime(i)){
-            printf("%i",i);
+            printf("%i\n",i);
         }
     }
 
 }
 
 bool prime(int i){
-    if(i==1){
+    if(i<=1){
         return false;
     }
     if(i==2){
         return true;
     }
-    float y = 0.0;
-    for(int u=3; u<i; u++){
-
-        y += i%u;
-    }
-    if (y==0){
+    // 如果数值是偶数且大于 2，则不是质数
+    if (i % 2 == 0) {
         return false;
     }
-    else{
-        return true;
+    float y = 0.0;
+    for(int u=3; u<i; u++){
+         if (i % u == 0) {
+            return false;
+        }
     }
-
+     return true;
 
 }
 
