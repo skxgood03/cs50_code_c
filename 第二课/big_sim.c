9846e7131ce145d589aa762ec8fd01271fceee6d
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-18 08:35:37 +0000

    Mon, Nov 18, 2024, 4:35 PM +08:00

---
+++ 第二课/big_sim.c
@@ -0,0 +1,15 @@
+#include<cs50.h>
+#include<stdio.h>
+#include<string.h>
+int main(){
+    string s = get_string("输入随机英文转大写：")
+    for(int i=0,n = strlen(n); i<n; i++){
+        if(s[i]>="a" && s[i]<="z"]){
+            printf("%c"，s[i]-("a"-"A"));
+        }else{
+            printf("%c",s[i]);
+        }
+    }
+    printf("\n");
+
+}
