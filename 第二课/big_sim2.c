704cea6366eb9c9bc12c7b08b13b818093a49456
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-18 09:09:35 +0000

    Mon, Nov 18, 2024, 5:09 PM +08:00

--- 第二课/big_sim2.c
+++ 第二课/big_sim2.c
@@ -7,15 +7,8 @@ int main()
     string s = get_string("输入随机英文转大写：");
     for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(islower(s[i]))
-        {
-            //ASCII 值差 大小写值差为32
+
             printf("%c",toupper(s[i]));
-        }
-        else
-        {
-            printf("%c", s[i]);
-        }
     }
     printf("\n");
 }
