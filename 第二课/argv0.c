0cb83b9b964000ffc0e16c30aa7c3b8edda6b3f0
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-20 06:27:21 +0000

    Wed, Nov 20, 2024, 2:27 PM +08:00

--- 第二课/argv0.c
+++ 第二课/argv0.c
@@ -3,6 +3,8 @@
 
 int main(int argc,string argv[]){
     printf("argc是%i，argv0是%s,argv1是%s",argc,argv[0],argv[1]);
+    //在运行程序./argv0 时argc是1    argv[0]是“./argv0”
+    // 当./argv0 xxx 时  argc是2（）argv[0]是“./argv0”argv[1]是“xxx”
     if(argc==2){
         printf("hello,%s\n",argv[1]);
     }
