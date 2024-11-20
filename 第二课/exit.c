a76573f6cc332adb9e2045cfda5b73339d0262c8
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-20 07:51:31 +0000

    Wed, Nov 20, 2024, 3:51 PM +08:00

--- 第二课/exit.c
+++ 第二课/exit.c
@@ -8,7 +8,7 @@ int main(int argc,string argv[]){
         // 非0的返回一律是有问题的
         return 1;
     }
-    else{
+    else{ 
         printf("hello,%s\n",argv[1]);
         // 可不写 默认返回0
         return 0;
