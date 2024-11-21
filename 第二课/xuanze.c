d3db5033b2d69fd563f7c4f81cdeb6a5dc9a7bee
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-21 07:47:55 +0000

    Thu, Nov 21, 2024, 3:47 PM +08:00

--- 第二课/xuanze.c
+++ 第二课/xuanze.c
@@ -9,8 +9,9 @@ int main(){
         int min = arr[i];
         for(int j=0;j<length+1;j++){
             if (arr[j]<arr[min]){
+                min = j
                 int tem = arr[j];
-                arr[j] = tem
+                arr[j] = tem;
             }
         }
     }
