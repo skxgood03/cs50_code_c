04be80a323569cfffe218d05ad64cf77a82c02fb
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-21 07:52:54 +0000

    Thu, Nov 21, 2024, 3:52 PM +08:00

--- 第二课/xuanze.c
+++ 第二课/xuanze.c
@@ -1,18 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-
-int main(){
-    int arrt[] = {3,4,6,8,0};
+int main()
+{
+    int arrt[] = {3, 4, 6, 8, 0};
     int length = sizeof(arrt) / sizeof(arrt[0]);
-    for(int i=0;i<length; i++){
-        int min = arr[i];
-        for(int j=0;j<length+1;j++){
-            if (arr[j]<arr[min]){
-                min = j
-                int tem = arr[j];
-                arr[j] = tem;
+    for (int i = 0; i < length; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < length; j++)
+        {
+            if (arrt[j] < arrt[min])
+            {
+                min = j;
             }
+            int tmpe = arrt[min];
+            arrt[min] = arrt[j];
+            arrt[j] = tmpe;
         }
     }
+    printf("整体数组：");
+    for (int i = 0; i < length; i++)
+    {
+        printf("%i", arrt[i]);
+    }
+    printf("\n");
 }
