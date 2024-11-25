0c5becad5999404d97e5167e1900e3f9183df6fc
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-25 07:39:41 +0000

    Mon, Nov 25, 2024, 3:39 PM +08:00

---
+++ 第二课/games.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    string p1 = get_string("请输入你的字母：");
+    string p2 = get_string("请输入你的字母：");
+    printf("%s,%s",p1,p2);
+
+}
+
