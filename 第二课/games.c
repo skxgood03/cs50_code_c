5b6ff45932a1ddff2be6f062f4c109de52addbea
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-25 07:53:15 +0000

    Mon, Nov 25, 2024, 3:53 PM +08:00

--- 第二课/games.c
+++ 第二课/games.c
@@ -2,6 +2,8 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
+
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 int compute_score(string word) ;
 int main()
 {
