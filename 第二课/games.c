82eeef264d5787801591caa5064fde39b33cbd35
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-25 07:52:37 +0000

    Mon, Nov 25, 2024, 3:52 PM +08:00

--- 第二课/games.c
+++ 第二课/games.c
@@ -1,12 +1,42 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
-
-int main(){
+int compute_score(string word) ;
+int main()
+{
     string p1 = get_string("请输入你的字母：");
     string p2 = get_string("请输入你的字母：");
-    printf("%s,%s",p1,p2);
 
+    int scores1 = compute_score(p1);
+    int scores2 = compute_score(p2);
+    if (scores1 > scores2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (scores1 < scores2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("平局");
+    }
 }
 
+int compute_score(string word)
+{
+    int score = 0;
+    for (int i = 0, len = strlen(word); i < len; i++)
+    {
+        if (isupper(word[i]))
+        {
+            score += POINTS[word[i] - 'A'];
+        }
+        else if (islower(word[i]))
+        {
+            score += POINTS[word[i] - 'a'];
+        }
+    }
+    return score;
+}
