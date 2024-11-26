f91efb060d7975c6dc5598542e74a05b8b1862a4
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-26 10:36:30 +0000

    Tue, Nov 26, 2024, 6:36 PM +08:00

--- 第二课/readability.c
+++ 第二课/readability.c
@@ -4,15 +4,35 @@
 #include <stdio.h>
 #include <string.h>
 int lenght_text(string text);
+int lenght_dc(string text);
 int main(void)
 {
     string text = get_string("请输入文本：");
     int len = lenght_text(text);
+    int len_dc = lenght_dc(text);
     printf("文本中的字母数：%i\n",len);
+    printf("文本中的单词数：%i\n",len_dc);
 
 }
 
 int lenght_text(string text){
     int len = strlen(text);
     return len;
+}
+
+int lenght_dc(string text){
+
+    int len_dc = 0;
+    for(int i=0,len = strlen(text);i<len;i++){
+        if(text[i]==""){
+            len_dc ++
+        }
+    }
+    int len = strlen(text);
+    if(len_dc==0 ||len==0){
+        len_dc = 0
+    }else{
+        len_dc = 1
+    }
+    return len_dc;
 }
