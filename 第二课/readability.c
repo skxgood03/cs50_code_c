3ad4f0b3040cbca3a98a7332e4099c84d034dbd2
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-26 10:27:13 +0000

    Tue, Nov 26, 2024, 6:27 PM +08:00

---
+++ 第二课/readability.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+int lenght_text(string text);
+int main(void)
+{
+    string text = get_string("请输入文本：");
+    int len = lenght_text(text);
+    printf("文本中的字母数：%s\n",len);
+
+}
+
+int lenght_text(string text){
+    int len = strlen(text);
+    return len;
+}
