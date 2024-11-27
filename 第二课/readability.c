3fb6fc939db731880fe271db04b68bdbf577ac66
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-27 02:57:14 +0000

    Wed, Nov 27, 2024, 10:57 AM +08:00

--- 第二课/readability.c
+++ 第二课/readability.c
@@ -4,85 +4,47 @@
 #include <stdio.h>
 #include <string.h>
 int lenght_text(string text);
-int lenght_dc(string text);
-int lenght_jz(string text);
 int main(void)
 {
     string text = get_string("请输入文本：");
     int len = lenght_text(text);
-
-    printf("文本中的字母数：%i\n", len);
 }
 
 int lenght_text(string text)
 {
-// 初始化计数器
+    // 初始化计数器
     int num_letters = 0;
     int num_words = 0;
     int num_sentences = 0;
 
     // 标记是否在单词中
     int in_word = 0;
-    for (int i = 0,n = strlen(text); i < len; i++){
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
         if (isalpha(text[i]))
         {
-            num_letters ++;
-            if (!in_word) {
+            num_letters++;
+            if (!in_word)
+            {
                 num_words++;
-                in_word = 1;  // 进入单词状态
-            }else {
-            in_word = 0;  // 离开单词状态
-            if (text[i] == '.' || text[i] == '!' || text[i] == '?') {  // 是句子的结束标志
-                num_sentences++;
+                in_word = 1; // 进入单词状态
             }
-        }
-    }
-}
-
-int lenght_dc(string text)
-{
-
-    int len_dc = 1;
-    int len = strlen(text);
-    for (int i = 0; i < len; i++)
-    {
-    }
-    if (len == 0)
-    {
-        len_dc = 0;
-    }
-    return len_dc;
-}
-int lenght_jz(string text)
-{
-
-    int len_jz = 1;
-    int len_dc = 1;
-    int len = strlen(text);
-    for (int i = 0; i < len; i++)
-    {
-        if (isalpha(text[i]))
-        {
-            if (text[i] == ' ')
+            else
             {
-                len_dc++;
-            }
-        }
-        else
-        {
-
-            if (text[i] == '!' || text[i] == '.' || text[i] == '?')
-            {
-                len_jz++;
+                in_word = 0; // 离开单词状态
+                if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+                { // 是句子的结束标志
+                    num_sentences++;
+                }
             }
         }
     }
-    if (len == 0)
+    if (num_words == 0)
     {
-        len_jz = 0;
-        len_dc = 0;
+        printf("文本中没有找到单词。\n");
+        return 1;
     }
-    printf("文本中的单词数：%i\n", len_dc);
-    printf("文本中的句子：%i\n", len_jz);
-    return len_jz;
+    printf("文本中的字母数：%i\n", num_letters);
+    printf("文本中的单词数：%i\n", num_words);
+    printf("文本中的句子数：%i\n", num_sentences);
 }
