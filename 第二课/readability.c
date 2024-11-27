7dfdeec74b7156b6c99da725c78b61c6b8682b2f
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-27 02:50:37 +0000

    Wed, Nov 27, 2024, 10:50 AM +08:00

--- 第二课/readability.c
+++ 第二课/readability.c
@@ -1,5 +1,5 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <math.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,49 +12,59 @@ int main(void)
     int len = lenght_text(text);
     int len_dc = lenght_dc(text);
     int len_jz = lenght_jz(text);
-    printf("文本中的字母数：%i\n",len);
-    printf("文本中的单词数：%i\n",len_dc);
-    printf("文本中的句子：%i\n",len_jz);
+    printf("文本中的字母数：%i\n", len);
 }
 
-int lenght_text(string text){
+int lenght_text(string text)
+{
     int len = strlen(text);
     return len;
 }
 
-int lenght_dc(string text){
+int lenght_dc(string text)
+{
 
     int len_dc = 1;
     int len = strlen(text);
-    for(int i=0;i<len;i++){
-
-        if(text[i]==' ' ){
-            len_dc ++;
-        }
+    for (int i = 0; i < len; i++)
+    {
     }
-    if(len==0){
+    if (len == 0)
+    {
         len_dc = 0;
     }
     return len_dc;
 }
-int lenght_jz(string text){
+int lenght_jz(string text)
+{
 
     int len_jz = 1;
+    int len_dc = 1;
     int len = strlen(text);
-    for(int i=0;i<len;i++){
-        if (isalpha(text[i])){
-
-        }else{
-
-        if(text[i]=='!' ||text[i]=='.'|| text[i]=='?' ){
-            len_jz ++;
+    for (int i = 0; i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            if (text[i] == ' ')
+            {
+                len_dc++;
+            }
         }
-        }
+        else
+        {
 
+            if (text[i] == '!' || text[i] == '.' || text[i] == '?')
+            {
+                len_jz++;
+            }
+        }
     }
-    if(len==0){
+    if (len == 0)
+    {
         len_jz = 0;
+        len_dc = 0;
     }
+    printf("文本中的单词数：%i\n", len_dc);
+    printf("文本中的句子：%i\n", len_jz);
     return len_jz;
 }
-
