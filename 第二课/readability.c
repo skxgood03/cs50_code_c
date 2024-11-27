6db1d4d615c533e852b83503a0fd45a1a426a527
ethan <63276035+skxgood03@users.noreply.github.com>
2024-11-27 02:42:47 +0000

    Wed, Nov 27, 2024, 10:42 AM +08:00

--- 第二课/readability.c
+++ 第二课/readability.c
@@ -43,7 +43,7 @@ int lenght_jz(string text){
     int len = strlen(text);
     for(int i=0;i<len;i++){
 
-        if(text[i]=='!' ||text[i]=='.' text[i]=='?' ){
+        if(text[i]=='!' ||text[i]=='.'|| text[i]=='?' ){
             len_jz ++;
         }
     }
